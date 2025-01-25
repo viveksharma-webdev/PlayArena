b7ac9da3688b8f173eeb9a4b1af2a132fbf577ba
Vivek Sharma <vivek45559s@gmail.com>
2025-01-25 16:39:04 +0530

    Third commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
